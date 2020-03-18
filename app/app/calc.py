1531748a2be760b18f5c387d5a7282d63cd45eea
rahulsharma1311 <rahul.sharma15786@yahoo.com>
2020-03-18 14:33:10 +0530

    Added substract method in test.py and test function fot it.

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,3 +3,7 @@
 def add(x, y):
     """Add two numbers together."""
     return x + y
+
+def substract(x, y):
+    """substract two numbers."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,3 +7,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are added together."""
         self.assertEqual(add(3, 8), 11)
+
+    def test_substract_numbers(self):
+        """Test that two numbers substracted properly."""
+        self.assertEqual(substract(3, 10), 7)
